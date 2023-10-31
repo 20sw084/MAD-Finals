f6aba7904674f493786a80d2327bdbb1a1c7b5b2
Junaid Aslam <junaidaslam.muet@gmail.com>
2023-10-31 12:14:30 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'package:finals_practice/firebase_example.dart';
 import 'package:finals_practice/current_location.dart';
 import 'package:finals_practice/google_maps.dart';
 import 'package:finals_practice/provider_exmple.dart';
 import 'package:finals_practice/rest_apis.dart';
 import 'package:finals_practice/shared_preferences_code.dart';
 import 'package:finals_practice/sqflite_example.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-main() {
+import 'firebase_options.dart';
+
+main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
@@ -29,7 +36,7 @@ class Mypp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CurrentLocation(),
+      home: CloudFirestoreExample(),
     );
   }
 }
